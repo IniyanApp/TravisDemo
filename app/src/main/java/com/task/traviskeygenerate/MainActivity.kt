b557201efcf024cb1df759ec046fe82dcfe54940
iniyan m <iniyan.m@krazybee.com>
2020-04-14 12:28:50 +0530

    travis setup

--- app/src/main/java/com/task/traviskeygenerate/MainActivity.kt
+++ app/src/main/java/com/task/traviskeygenerate/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.task.traviskeygenerate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+import com.task.traviskeygenerate.BuildConfig;
 
 class MainActivity : AppCompatActivity() {
+    var TAG = MainActivity::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val gatewayUrl: String = BuildConfig.GATEWAY_KEY
+        val baseUrl: String = BuildConfig.TRAVIS_BASEURL
+
+        Log.e(TAG, "==gateway=$gatewayUrl")
+        Log.e(TAG, "==baseUrl=$baseUrl")
     }
 }
